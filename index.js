e30bd3bfd9fb713f87947cd56469cf35ebf933b9
Nishith Gohel <201901122@daiict.ac.in>
2023-10-04 20:53:57 +0530

    Modified the style and implemented histroy trackking functionality

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const operators = document.querySelectorAll('.oper');
 const equal_btn = document.querySelector('.equal');
 const clr = document.querySelector('.clear');
 const ce_btn = document.querySelector('.ce_clear');
+const backspace = document.querySelector('.backspace');
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const deleteHistory = document.querySelector(".delete-history");
 
 let dis1_num = '';
 let dis2_num = '';
@@ -72,6 +76,33 @@ function calculation(){
     } else if (last_oper === '%') {
         result = parseFloat(result) % parseFloat(dis2_num)
     }
+
+    saveToHistory();
+}
+
+function saveToHistory(){
+    const expression = dis1_num + dis2_num;
+    const historyItem = {expression, result}
+    const history = JSON.parse(localStorage.getItem("history")) || [];
+    history.push(historyItem);
+    localStorage.setItem('history', JSON.stringify(history));
+    updateHistory();
+}
+
+function updateHistory() {
+    const history = JSON.parse(localStorage.getItem("history")) || [];
+    const historyContent = document.querySelector(".history-content");
+    history.forEach((item, index) => {
+        const historyEntry = document.createElement('div');
+        historyEntry.textContent = `${item.expression} = ${item.result}`;
+        historyContent.appendChild(historyEntry);
+    });
+}
+
+function eraseHistory() {
+    const history = JSON.parse(localStorage.getItem("history")) || [];
+    const historyContent = document.querySelector(".history-content");
+    historyContent.innerHTML = "";
 }
 
 // On click of "=" button will perform calculation and display on the screen, calculated result
@@ -97,10 +128,38 @@ clr.addEventListener('click', (e)=>{
 })
 
 // Clicking CE button will clear last operand
-ce_btn.addEventListener('click', (e)=>{             
-    display2.innerText = '';
+ce_btn.addEventListener('click', (e)=>{ 
+    display1.innerText = '';
+    display2.innerText = '0';
     dis2_num = '';
 });
 
+// Erase element on backspace
+backspace.addEventListener('click', () => {
+    console.log("Backspace clicked")
+    dis2_num = dis2_num.slice(0, -1);
+    display2.innerText = dis2_num;
+})
 
 
+hamburger.addEventListener('click', () => {
+    // console.log("hamburger displayed")
+    const historyDiv = document.querySelector(".history"), calc = document.querySelector(".calc-wrapper"), content = document.querySelector(".history-content");
+    historyDiv.style.display = "block";
+    calc.style.display = "none";
+})
+
+// Toggling the histoy panel view 
+
+close.addEventListener("click", () => {
+    const historyDiv = document.querySelector(".history"), calc = document.querySelector(".calc-wrapper");
+    historyDiv.style.display = "none";
+    calc.style.display = "block";
+})
+    
+deleteHistory.addEventListener("click", () => {
+    const history = JSON.parse(localStorage.getItem("history")) || [];
+    history = [];
+    localStorage.setItem('history', JSON.stringify(history));
+    eraseHistory();
+})
